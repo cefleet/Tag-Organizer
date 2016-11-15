d2bd1fde50a59e0a25c99d64b7a2ee73ecbc3c12
Clint Fleetwood <clint.fleetwood@gmail.com>
2016-11-15 10:45:27 -0500

    added display names

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.get('/getImages/', function (req, res) {
   var sql = ''
   console.log(tagArray);
   if(tagArray.length == 0){
-    sql = "SELECT Images.id,Images.name FROM Images"
+    sql = "SELECT Images.id,Images.name,ImageComments.comment as displayName FROM Images JOIN ImageComments ON Images.id = ImageComments.imageid"
   } else {
-    var section = "SELECT Images.id,Images.name FROM Images,Tags,ImageTags WHERE ImageTags.imageid=Images.id AND ImageTags.tagid=Tags.id AND Tags.name";
+    var section = "SELECT Images.id,Images.name,ImageComments.comment as displayName FROM Images,Tags,ImageTags JOIN ImageComments ON Images.id = ImageComments.imageid WHERE ImageTags.imageid=Images.id AND ImageTags.tagid=Tags.id AND Tags.name";
     for(var i = 0; i< tagArray.length; i++){
       if(i > 0){
         sql = sql+' INTERSECT ';
@@ -43,6 +43,9 @@ app.get('/getImages/', function (req, res) {
   }
 //  var sql = "SELECT Images.id,Images.name FROM Images,Tags,ImageTags WHERE ImageTags.imageid=Images.id AND ImageTags.tagid=Tags.id AND Tags.name ('1ph') INTERSECT SELECT Images.id,Images.name FROM Images,Tags,ImageTags WHERE ImageTags.imageid=Images.id AND ImageTags.tagid=Tags.id AND Tags.name IN ('ecoga') INTERSECT SELECT Images.id,Images.name FROM Images,Tags,ImageTags WHERE ImageTags.imageid=Images.id AND ImageTags.tagid=Tags.id AND Tags.name IN ('2011')";
   db.all(sql, function(err, data){
+    if(err){
+      console.log(err)
+    }
     res.send(data)
   })
 });
